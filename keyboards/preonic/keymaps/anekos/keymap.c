f7999cba47cf67af2eae88e1dfccaee65bde335e
anekos <anekos@snca.net>
2016-08-15 21:35:18 +0900

    anekos.h

--- keyboards/preonic/keymaps/anekos/keymap.c
+++ keyboards/preonic/keymaps/anekos/keymap.c
@@ -4,6 +4,7 @@
 #ifdef AUDIO_ENABLE
   #include "audio.h"
 #endif
+#include "anekos.h"
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -41,21 +42,8 @@ const qk_tap_dance_action_t tap_dance_actions[] = {
   // [TD_QUOTE]  = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, RSFT(KC_QUOT))
 };
 
-/**
- * Shift-and-Space は、「<Space><Space>おしっぱなし」とすることでキーリピートを発生させることができる。
- */
-
-// XandY
-#define MY_COLN CTL_T(KC_SCLN)
-#define CT_SPC  SFT_T(KC_SPC)
-#define BT_ESC  LT(_LOWER, KC_ESC)
-#define BT_ENT  LT(_RAISE, KC_ENT)
-#define TP_ESC  LT(_EXT, KC_ESC)
-// Alias
-#define KC_LT   LSFT(KC_COMM)
-#define KC_GT   LSFT(KC_DOT)
 // Tap Dance
-#define MY_QUOT TD(TD_QUOTE)
+#define TD_QUOT TD(TD_QUOTE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -73,11 +61,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {TP_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS},
+  {EX_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS},
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV},
-  {KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    MY_COLN, MY_QUOT},
+  {KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    C_COLN,  TD_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {BT_ESC,  _______, _______, _______, KC_LALT, CT_SPC,  CT_SPC,  KC_RALT, _______, _______, _______, BT_ENT}
+  {LO_ESC,  _______, _______, _______, A_ESC,   S_SPC,   S_SPC,   A_ESC,   _______, _______, _______, RA_ENT}
 },
 
 /* Lower
