066ef3437fff9330bec2181055baa374c421915b
anekos <anekos@snca.net>
2017-10-19 19:33:14 +0900

    Add Dynamic macro

--- keyboards/ergodox_ez/keymaps/anekos/rules.mk
+++ keyboards/ergodox_ez/keymaps/anekos/rules.mk
@@ -4,6 +4,7 @@ COMMAND_ENABLE    = no  # Commands for debug and configuration
 RGBLIGHT_ENABLE   = yes
 MIDI_ENABLE       = no
 CONSOLE_ENABLE    = yes
+TERMINAL_ENABLE   = no
 
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
