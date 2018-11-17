adfb7d890544c3e4c2f3ce0bb7c879891585a2aa
anekos <anekos@snca.net>
2018-11-17 21:32:09 +0900

    Meow

--- keyboards/nyquist/keymaps/anekos/keymap.c
+++ keyboards/nyquist/keymaps/anekos/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [LR0] = LAYOUT( \
   L5_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV,  \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    L4_GRV,  \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    C_COLN,  KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
   MO_1,    MO_4,    A_ESC,   A_ESC,   A_ESC,   S_SPC,   S_SPC,   A_ESC,   A_ESC,   A_ESC,   _______, MO_2    \
