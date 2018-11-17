f4fa977eb3cbc084566c9efa7bdfc36eb815e71f
anekos <anekos@snca.net>
2018-11-17 21:10:25 +0900

    Update for Kinesis

--- keyboards/kinesis/keymaps/anekos/keymap.c
+++ keyboards/kinesis/keymaps/anekos/keymap.c
@@ -38,29 +38,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 *                                        | Ctrl | Alt  |         | Gui  | Ctrl |
 *                                 ,------|------|------|         |------+------+------.
 *                                 |      |      | Home |         | PgUp |      |      |
-*                                 | A/Esc| S/Spc|------|         |------|S/Spc |A/Esc |
+*                                 | S/Spc| A/Esc|------|         |------|A/Esc |S/Spc |
 *                                 |      |      | End  |         | PgDn |      |      |
 *                                 `--------------------'         `--------------------'
 */
 [LR0] = LAYOUT(
    KC_ESC ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8,
    KC_EQL ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,
-   KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,
+   L1_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,
    KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,
    KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,
            MO_1   ,KC_INS ,KC_LEFT,KC_RGHT,
-                           KC_LCTL,KC_LALT,
+                          AS(KC_G),AS(KC_G),
                                    KC_HOME,
-                   A_ESC  ,S_SPC  ,KC_END ,
+                   S_SPC  ,A_ESC  ,KC_END ,
     KC_F9 ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,KC_SLCK  ,KC_PAUS, KC_FN0, KC_1,
     KC_6  ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_MINS,
     KC_Y  ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSLS,
     KC_H  ,KC_J   ,KC_K   ,KC_L   ,C_COLN, KC_QUOT,
     KC_N  ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_ENT ,
            KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,
-           KC_RGUI,KC_RCTL,
+        AS(KC_ENT),AS(KC_ENT),
            KC_PGUP,
-           S_SPC  ,  S_SPC,   A_ESC
+           S_SPC  ,  A_ESC,   S_SPC
     ),
 /****************************************************************************************************
 *
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 *                                        |      |      |         |      |      |
 *                                 ,------|------|------|         |------+------+------.
 *                                 |      |      |      |         |      |      |      |
-*                                 | A/Esc| S/Spc|------|         |------|S/Spc |A/Esc |
+*                                 | S/Spc| A/Esc|------|         |------|A/Esc |S/Spc |
 *                                 |      |      |      |         |      |      |      |
 *                                 `--------------------'         `--------------------'
 */
@@ -97,6 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             _______,_______,
                                     _______,
                     _______,_______,_______,
+
     _______,_______,_______,_______,_______,_______,_______,_______,_______,
     _______,_______,KC_LCBR,KC_RCBR,_______,KC_BSPC,
     _______,_______,KC_LBRC,KC_RBRC,_______,KC_DEL ,
