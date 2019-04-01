77123083dcf6711d1bd7b0120c63b0ab623da599
anekos <anekos@snca.net>
2019-04-01 21:18:00 +0900

    Meowlify

---
+++ keyboards/idobo/keymaps/anekos/rules.mk
@@ -0,0 +1,4 @@
+MOUSEKEY_ENABLE = yes      # Mouse keys(+4700)
+COMMAND_ENABLE = no        # Commands for debug and configuration
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+TAP_DANCE_ENABLE  = yes
